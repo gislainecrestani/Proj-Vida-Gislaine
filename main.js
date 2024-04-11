a2392ec7148e10d1b3ccf813e0672dc50ba2ef02
gislainecrestani <107581676+gislainecrestani@users.noreply.github.com>
2024-04-11 17:28:57 -0300

    11/04/2024

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+
+
+
+
+
+
+
+
+}
